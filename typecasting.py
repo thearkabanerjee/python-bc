2c710de2fe72d293c193acbccbabece91b1d7be4
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-27 19:09:20 +0530

    python is pythoning

---
+++ typecasting.py
@@ -0,0 +1,1 @@
+## this is all about just typecasting everything and also 
--- variable.py
+++ variable.py
@@ -6,10 +6,17 @@
 # print ('hello ', name)
 
 
-age: int = 21
-age += 1
+# age: int = 21
+# age += 1
 
-print (age)
+# print (age)
 
-print ('i am', age)
-print (type(age))
+# print ('i am', age)
+# print (type(age))
+
+height = 250.5
+
+print ("your height is" + str(height))
+
+print (type(height)) # float data type
+
