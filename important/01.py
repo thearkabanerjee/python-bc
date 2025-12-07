abde5df9e507e0ae9ad04ed0f59d58a5aeec91d1
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-12-07 12:06:34 +0530

    printing patterns today

---
+++ important/01.py
@@ -0,0 +1,30 @@
+# find all prime numbers less than the entered number
+
+number : int = int (input ('Enter the number you want: '))
+
+
+
+
+
+
+
+# # prime numbers 
+# for i in range (1, number+1):
+#     if (number % i == 0):
+#         counter += 1
+#         print (i)
+
+# if (counter == 2):
+#     print ('prime')
+# else:
+#     print ('non prime')
+# i = 0
+# while (i <number):
+#     for j in range (2, number):
+#         if (i % j != 0):
+#             counter += 1
+#             print (j)
+          
+#     i +=1 
+
+
