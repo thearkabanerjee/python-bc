face1902581e9085fc3aef2472c10955eb34a18e
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-12-08 14:00:52 +0530

    py we go

---
+++ important/dictionaries.py
@@ -0,0 +1,16 @@
+# not me learning dictionaries the first time
+
+capitals = {'USA':"Washington DC", "India": "New Delhi"}
+print (capitals.get('USA'))
+
+# to add to a dictionary 
+
+capitals.update({'China': 'Beijing'}) # update
+
+
+# update can be used to change it as well
+
+# capitals.update()
+
+print (capitals.keys())
+print (capitals.values())
