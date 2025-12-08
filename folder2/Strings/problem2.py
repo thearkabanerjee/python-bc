732b8d19a68ea4da2a8981c1a8928bcf5024e804
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-12-08 23:06:19 +0530

    strings/problem 2

---
+++ folder2/Strings/problem2.py
@@ -0,0 +1,11 @@
+# problem 2
+
+# Even Characters first and Odd Characters reversed
+# Given a string, return a string with the characters in the even indices first and the characters in the odd indices next but in reversed order.
+
+# Example For the input "abcde",
+
+# even chars = "ace"
+# odd chars = "bd"; odd chars reversed = "db"
+# result = "acedb"
+
